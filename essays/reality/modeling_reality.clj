20f52aad4a8e079fb84a60d7bd63b882f5ccfcbb
Sam Ritchie <sam@mentat.org>
2023-05-11 10:11:09 -0600

    second two notes

---
+++ essays/reality/modeling_reality.clj
@@ -0,0 +1,35 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns reality.modeling-reality
+  {:nextjournal.clerk/toc true})
+
+;; ## Modeling Reality
+
+;; Let's motivate what we're doing here. Our goal is to model reality. This is a
+;; deeply fascinating thing, that we can describe a system, and write down rules
+;; for how it evolves over time.... and then we can check the numbers for the
+;; real system later, and they will match.
+
+;; Realizing that this was possible, that there were "laws" that govern the way
+;; things evolve and that you could make predictions, is a major attack on the
+;; question of what the hell is going on all around us. It leads to the
+;; probably-safe idea that all sorts of abstractions can be modeled.
+
+;; Give an example of the sort of thing I'm talking about, with modeling of
+;; reality. We could do a pendulum example here, and maybe show it evolve.
+
+;; Another bizarre thing is that this modeling works at many levels of reality,
+;; and breaks down at others. You can model gases etc using probabilistic laws
+;; that break down when you isolate individual particles. And, surprise, it
+;; seems like the classical laws we'll investigate first are expected values of
+;; very different phenomena at the bottom.
+
+;; Another example here?
+
+;; And then of course we have mathematical abstractions that don't seem to
+;; correspond to any reality! But they hang together cohesively, and are worth
+;; studying. Do these form the bases of other realities? [[Max Tegmark]] seems
+;; to think so. But following the implications of structure turns out to be a
+;; nice way to find interesting behavior.
+
+;; Why some laws vs others? Talk about the various rules suggested by setting
+;; the dials to different settings.
