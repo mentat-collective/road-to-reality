de44991a2587870a34b6ac547f83f6c5ed77e2da
Sam Ritchie <sam@mentat.org>
2023-05-11 10:10:41 -0600

    intro

--- essays/reality/introduction.clj
+++ essays/reality/introduction.clj
@@ -1,8 +1,42 @@
-^#:nextjournal.clerk
-{:toc true
- :visibility :hide-ns}
-(ns reality.introduction)
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns reality.introduction
+  {:nextjournal.clerk/toc true})
 
 ;; ## Welcome to the Road to Reality!
 ;;
 ;; Still nothing here, but I am getting started.
+
+;; Introduce here what I'm trying to do. Fascination with these books, and with
+;; our ability to model reality. I've chosen and built a set of tools to do this
+;; that are maximally extensible you should be able to read these in the same
+;; environment where I wrote them, typesetting any changes you make with very
+;; little effort.
+
+;; And you should be able to go off in directions that I had never imagined
+;; before, and trivially share those changes back out to me.
+
+;; Of course nothing is actually trivial, but it's time to kick the tires on this
+;; round of tools and see where we can go.
+
+;; We are going to start with a Lisp interpreter, and build a powerful, modern
+;; computer algebra system capable of solving Einstein's field equations. Some of
+;; this tooling exists, and some of it we'll be building together in support of
+;; the essays.
+
+;; The algebra system lives at blah. Some of the essays are going to build up
+;; simpler versions of tools in that library. Some of the essays will be almost
+;; identical to the way the tools have been built in that library!
+
+;; I have an idea of where we're going, but I expect to drag you off in all sorts
+;; of interesting mathematical and historical directions. The difference between
+;; this and your typical mathematical blog or book is that everything we explore
+;; will be usable by you, potentially in your own work, potentially in a personal
+;; playground that you can model off of these essays.
+
+;; I have dreams about a collaborative version of all of this that you can read
+;; about at blah. That will all come once we get some of the essays going.
+
+;; In the meantime, the best I will be able to do is include this repository,
+;; with instructions on how you can follow along in your own sort of scratchpad.
+;; If you really want to get involved I will suggest a few exercises in each
+;; essay. Remember, we are on alpha version, so expect these to change!
